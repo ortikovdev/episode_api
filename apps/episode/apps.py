be6d47055d663ab02de07d50857083f3ea6fe964
anvar <ortikovua@gmail.com>
2024-03-18 09:42:44 +0500

    modified

--- apps/episode/apps.py
+++ apps/episode/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class EpisodeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'episode'
+    name = 'apps.episode'
--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    # 'rest_framework_swagger',
+    # 'psycopg2',
     'rest_framework.simplejwt',
     'drf_yasg',
 
@@ -117,9 +117,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
@@ -130,6 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
